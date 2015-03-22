c7a73b05f143cd95c7a23b07958b69f31a9ee1ee
Arthur Heezen <arthurheezen@gmail.com>
2015-03-22 12:02:53 -0500

    Tested code copy

--- cachematrix.R
+++ cachematrix.R
@@ -1,34 +1,50 @@
-## Put comments here that give an overall description of what your
-## functions do
+## This pair of functions extends a matrix definition to include
+## a mechanism for caching the inverse 
+## Usage: First call yourresult<-makeCacheMatrix(yourmatrix).
+##        Then call yourmatrixinverse<-cacheSolve(yourmatrix) for the inverse
+##        Repeated calls will use the cached copy of the inverse
 
-## Write a short comment describing this function
+## makeCacheMatrix extends a matrix
+##     arguments: 
+##         numeric matrix x
+##     returns:
+##         list object with data and accessor functions
+##             object$set(y = matrix())
+##             object$get(y = matrix())
+##             object$setsolve(y = matrix()) - Use cacheSolve function instead
+##             object$getsolve(y = matrix()) - Use cacheSolve function instead
 
 makeCacheMatrix <- function(x = matrix()) {
-
+    
     s <- NULL
-  
-        set <- function(y) {
-            x <<- y
-            s <<- NULL
-        }
-          
-        get <- function() x
-          
-        setsolve <- function(solve)  s <<- solve
-          
-        getsolve <- function() s
-          
-        list(set = set, get = get,
-            setsolve = setsolve,
-            getsolve = getsolve)
+    
+    set <- function(y) {
+        x <<- y
+        s <<- NULL
+    }
+    
+    get <- function() x
+    
+    setsolve <- function(solve)  s <<- solve
+    
+    getsolve <- function() s
+    
+    list(set = set, get = get,
+         setsolve = setsolve,
+         getsolve = getsolve)
 }
 
 
-## Write a short comment describing this function
+
+## cacheSolve returns the inverse of a matrix created with makeCacheMatrix
+##     arguments: 
+##         x - extended matrix object returned by "makeCacheMatrix"
+##     returns:
+##         inverse of the matrix stored in x.  Repeated call use a cached copy
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
-
+    ## Return a matrix that is the inverse of 'x'
+    
     s <- x$getsolve()
     if(!is.null(s)) {
         message("getting cached data")
@@ -39,5 +55,5 @@ cacheSolve <- function(x, ...) {
     x$setsolve(s)
     s
     
-
+    
 }
